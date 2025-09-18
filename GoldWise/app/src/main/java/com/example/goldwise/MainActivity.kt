180ddfaa1c7dd7712e042c430e72b361407c8f66
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 21:03:05 -0300

    Continuação APP Gold Wise

--- GoldWise/app/src/main/java/com/example/goldwise/MainActivity.kt
+++ GoldWise/app/src/main/java/com/example/goldwise/MainActivity.kt
@@ -9,19 +9,33 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,6 +57,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GoldWise(modifier: Modifier = Modifier) {
+
+    var quantity by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("") }
+    var resultado by remember { mutableStateOf("") }
+    var context = LocalContext.current
+    var avisoQtd by remember { mutableStateOf("") }
+    var avisoPrice by remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -66,6 +88,80 @@ fun GoldWise(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold
             )
         }
+
+        OutlinedTextField(
+            value = quantity,
+            onValueChange = {
+                quantity= it
+                if (quantity.isEmpty()){
+                    avisoQtd = "Preencha este campo"
+                } else{
+                    avisoQtd = ""
+                }
+                            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {Text("Quantidade")},
+            keyboardOptions = KeyboardOptions(
+                //usando teclado numerico
+                keyboardType = KeyboardType.Number
+            )
+        )
+        Text(avisoQtd, style = TextStyle(
+            color = androidx.compose.ui.graphics.Color.Red
+        ))
+
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedTextField(
+            value = price,
+            onValueChange = {
+                price = it
+                if (price.isEmpty()){
+                    avisoPrice = "Preencha este campo"
+                } else{
+                    avisoPrice = ""
+                }
+                            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {Text("Preço")},
+            keyboardOptions = KeyboardOptions(
+                //usando teclado numerico
+                keyboardType = KeyboardType.Decimal
+            )
+        )
+        Text(avisoPrice, style = TextStyle(
+            color = androidx.compose.ui.graphics.Color.Red
+        ))
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Button( //o que vai fazer
+            onClick = {
+                if (price.isEmpty() || quantity.isEmpty()){
+                    resultado = "Preencha todos os campos"
+                } else{
+                    val total = quantity.toDouble() * price.toDouble()
+                    resultado = context.getString(R.string.total, total)
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+
+        ) { //o que vai renderizar
+            Text("Calcular")
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+        OutlinedButton(
+            onClick = {
+                quantity = ""
+                price = ""
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Limpar")
+        }
+
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(resultado)
+
 
     }
 }
