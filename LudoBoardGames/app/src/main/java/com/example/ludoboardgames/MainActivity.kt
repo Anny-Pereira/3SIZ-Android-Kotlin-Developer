901d271b7b3df4568045b0b399852aab5106996f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-22 20:51:51 -0300

    Continuacao Projeto LudoBoardGames - Composable e Listas

--- LudoBoardGames/app/src/main/java/com/example/ludoboardgames/MainActivity.kt
+++ LudoBoardGames/app/src/main/java/com/example/ludoboardgames/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,11 +24,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ludoboardgames.repository.getAllBoardGames
+import com.example.ludoboardgames.repository.getAllGamesPublisher
+import com.example.ludoboardgames.repository.getBoardGameBy
+import com.example.ludoboardgames.repository.getBoardGamesBy
+import com.example.ludoboardgames.ui.components.BoardGameCardList
+import com.example.ludoboardgames.ui.components.GamePublisherCard
 import com.example.ludoboardgames.ui.components.GameSearchBar
 import com.example.ludoboardgames.ui.theme.LudoBoardGamesTheme
 
@@ -65,6 +80,11 @@ fun GameScreen(modifier: Modifier = Modifier) {
     var searchTextState by remember { mutableStateOf("") }
     var searchExampleState by remember { mutableStateOf("") }
 
+    //remember mantém a informação salva
+    var gamePublisherState by remember { mutableStateOf(getAllGamesPublisher()) }
+
+    var boardGamesListState by remember { mutableStateOf(getAllBoardGames()) }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -85,15 +105,61 @@ fun GameScreen(modifier: Modifier = Modifier) {
             },
             onSearch = {
                 // Realizar a busca
-                searchExampleState = it
+                boardGamesListState = getBoardGamesBy(it)
             },
             onClear = {
                 searchTextState = ""
                 searchExampleState = ""
+                boardGamesListState = getAllBoardGames()
             }
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text("Texto pesquisado: $searchExampleState")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyRow(
+            contentPadding = PaddingValues(
+                horizontal
+                = 16.dp
+            ),
+            horizontalArrangement =
+                Arrangement.spacedBy(16.dp)
+        ) {
+            items(gamePublisherState) { gamePublisher ->
+                GamePublisherCard(gamePublisher) {
+                    boardGamesListState = getBoardGameBy(it)
+                }
+            }
+        }
+
+        if (boardGamesListState.isEmpty()) {
+            // Código a ser exibido quando a lista estiver vazia
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Nenhum jogo encontrado",
+                    style =
+                        MaterialTheme.typography.bodyLarge,
+                    color =
+                        MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        } else {
+            // Código a ser exibido quando a lista estiver preenchida
+            Spacer(modifier = Modifier.height(16.dp))
+            LazyColumn(modifier =
+                Modifier.fillMaxWidth().padding(8.dp)) {
+                items(boardGamesListState) { game ->
+                    BoardGameCardList(game)
+                }
+            }
+        }
+
     }
 }
 
