f52b26275303c6224cedbaa0cde303d546a226fc
Anny-Pereira <isabelle.pereira04@gmail.com>
2025-11-10 12:15:29 -0300

    Tentativa LudoBoardGames c Imagens

--- LudoBoardGamesClass/app/build.gradle.kts
+++ LudoBoardGamesClass/app/build.gradle.kts
@@ -5,15 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "br.com.annypereira.ludoboardgames"
-    compileSdk {
-        version = release(36)
-    }
+    namespace = "com.example.ludoboardgames"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "br.com.annypereira.ludoboardgames"
+        applicationId = "com.example.ludoboardgames"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -42,19 +40,20 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
